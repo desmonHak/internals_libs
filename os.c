8510d14a019dc5394e55d7743da197804ca65711
desmonHak <61366533+desmonHak@users.noreply.github.com>
2025-05-18 19:44:12 +0200

    add

--- os.c
+++ os.c
@@ -5,24 +5,31 @@
 
 #include <stdio.h>
 
-
-
-DLL_EXPORT arch_type_t type_arch = arch_type_unknown;
+DLL_EXPORT info_destination_module info_module = {
+    .arch   = THIS_ARCH,
+    .os     = THIS_OS
+};
 
 DLL_EXPORT shellcode_t code;
 
 #include "init.c"
 
 static inline void os_constructor(void) { 
-    puts("hola desde el constructor"); 
+    puts("--- os_constructor ---"); 
+    printf("- type_arch: %s\n", arch_type_string[info_module.arch]);
+    printf("- type_os:   %s\n", os_type_string[info_module.os]);
 }
 static inline void os_destructor(void) { 
-    puts("hola desde el destructor"); 
+    puts("--- os_destructor  ---"); 
 }
 
-void register_os_init(void) {
+/**
+ * @brief permite registrar subrutinas
+ * constructoras y destructoras
+ */
+void register_init(void) {
     constructor = os_constructor;
-    destructor = os_destructor;
+    destructor  = os_destructor;
 }
 
 
@@ -36,4 +43,51 @@ DLL_EXPORT void who_system() {
 
     printf("who_system!\n");
     EMIT_MOV_R_I(ptr_sc, RBX, 0x12345678deadbeefull);
+}
+
+
+const char *getBuild() { //Get current architecture, detectx nearly every architecture. Coded by Freak
+    #if defined(__x86_64__) || defined(_M_X64)
+    return "x86_64";
+    #elif defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
+    return "x86_32";
+    #elif defined(__ARM_ARCH_2__)
+    return "ARM2";
+    #elif defined(__ARM_ARCH_3__) || defined(__ARM_ARCH_3M__)
+    return "ARM3";
+    #elif defined(__ARM_ARCH_4T__) || defined(__TARGET_ARM_4T)
+    return "ARM4T";
+    #elif defined(__ARM_ARCH_5_) || defined(__ARM_ARCH_5E_)
+    return "ARM5"
+    #elif defined(__ARM_ARCH_6T2_) || defined(__ARM_ARCH_6T2_)
+    return "ARM6T2";
+    #elif defined(__ARM_ARCH_6__) || defined(__ARM_ARCH_6J__) || defined(__ARM_ARCH_6K__) || defined(__ARM_ARCH_6Z__) || defined(__ARM_ARCH_6ZK__)
+    return "ARM6";
+    #elif defined(__ARM_ARCH_7__) || defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__)
+    return "ARM7";
+    #elif defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__)
+    return "ARM7A";
+    #elif defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__)
+    return "ARM7R";
+    #elif defined(__ARM_ARCH_7M__)
+    return "ARM7M";
+    #elif defined(__ARM_ARCH_7S__)
+    return "ARM7S";
+    #elif defined(__aarch64__) || defined(_M_ARM64)
+    return "ARM64";
+    #elif defined(mips) || defined(__mips__) || defined(__mips)
+    return "MIPS";
+    #elif defined(__sh__)
+    return "SUPERH";
+    #elif defined(__powerpc) || defined(__powerpc__) || defined(__powerpc64__) || defined(__POWERPC__) || defined(__ppc__) || defined(__PPC__) || defined(_ARCH_PPC)
+    return "POWERPC";
+    #elif defined(__PPC64__) || defined(__ppc64__) || defined(_ARCH_PPC64)
+    return "POWERPC64";
+    #elif defined(__sparc__) || defined(__sparc)
+    return "SPARC";
+    #elif defined(__m68k__)
+    return "M68K";
+    #else
+    return "UNKNOWN";
+    #endif
 }
--- os.h
+++ os.h
@@ -6,6 +6,12 @@
 
 #include "din_lib_c.h"
 
+/* version
+ * | version  | sub version |
+ * 0b0000 0001 0000 0000
+ */
+#define VERSION_OS_MODULE 0b0000 0001 0000 0000
+
 typedef enum arch_type_t {
     arch_type_x86_16,
     arch_type_x86_32,
@@ -14,6 +20,61 @@ typedef enum arch_type_t {
     arch_type_unknown
 } arch_type_t;
 
+static const char* arch_type_string[] = {
+    [arch_type_x86_16]  = "x86_16",
+    [arch_type_x86_32]  = "x86_32",
+    [arch_type_x86_64]  = "x86_64",
+    [arch_type_unknown] = "unknown"
+};
+
+/**
+ * @brief permite crear dos macros que indiquen la arquitectura y tamaño de la
+ * CPU en la que se compilo el codigo actual.
+ */
+#if defined(__x86_64__) || defined(_M_X64)
+    #define WORD_ARCH   64
+    #define THIS_ARCH   arch_type_x86_64
+#elif defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
+    #define WORD_ARCH   32
+    #define THIS_ARCH   arch_type_x86_32
+#elif defined(__16BIT__) || defined(_M_I86)
+    #define WORD_ARCH   16
+    #define THIS_ARCH   arch_type_x86_16
+#else
+    #error "Arquitectura desconocida. No se puede determinar si es 16, 32 o 64 bits."
+#endif
+
+typedef enum os_type_t {
+    os_type_windows,
+    os_type_linux,
+    os_type_mac,
+
+    os_type_unknown
+} os_type_t;
+
+#if defined(_WIN32) || defined(_WIN64)
+    #define THIS_OS os_type_windows
+#elif defined(__APPLE__) && defined(__MACH__)
+    #define THIS_OS os_type_windows
+#elif defined(__linux__)
+    #define THIS_OS os_type_linux
+#elif defined(__unix__) || defined(__unix)
+    #define THIS_OS os_type_mac
+#else
+    #define THIS_OS os_type_unknown
+#endif
+
+static const char* os_type_string[] = {
+    [os_type_windows]       = "windows",
+    [os_type_linux]         = "linux",
+    [os_type_mac]           = "mac",
+    [os_type_unknown]       = "unknown"
+};
+
+typedef struct info_destination_module {
+    os_type_t       os;
+    arch_type_t     arch;
+} info_destination_module;
 
 typedef void (*who_system_t)();
 
